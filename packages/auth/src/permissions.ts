fd16354f2b40386b76ba4dbf6571ec3a0d7cc773
Filipe Vieira <filipe.silva@bttis.com>
2024-08-22 17:30:34 -0300

    feat: finished defining permissions

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -10,10 +10,18 @@ type PermissionsByRole = (
 ) => void
 
 export const permissions: Record<Role, PermissionsByRole> = {
-  ADMIN(_, { can }) {
+  ADMIN(user, { can, cannot }) {
     can('manage', 'all')
+
+    cannot(['transfer_ownership', 'update'], 'Organization')
+    can(['transfer_ownership', 'update'], 'Organization', {
+      ownerId: {
+        $eq: user.id,
+      },
+    })
   },
   MEMBER(user, { can }) {
+    can('get', 'User')
     can(['create', 'get'], 'Project')
     can(['update', 'delete'], 'Project', {
       ownerId: {
@@ -21,5 +29,7 @@ export const permissions: Record<Role, PermissionsByRole> = {
       },
     })
   },
-  BILLING() {},
+  BILLING(_, { can }) {
+    can('manage', 'Billing')
+  },
 }
