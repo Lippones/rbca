da2cbf61ff3757ec79c7e969126f6ba604f790da
Filipe Vieira <filipe.silva@bttis.com>
2024-08-23 10:05:20 -0300

    feat: init prisma

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilitiesFor } from '@saas/auth'
-
-const ability = defineAbilitiesFor({
-  role: 'MEMBER',
-})
-
-const userCanManageAll = ability.can('manage', 'all')
-
-console.log(userCanManageAll)
